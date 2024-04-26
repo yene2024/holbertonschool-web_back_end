55ba1b518e6fa3a204ac283e367529c3202a7deb
yene2024 <yeneishla1524@gmail.com>
2024-04-26 11:56:52 -0400

    Import wait_random from 0-basic_async_syntax.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A way to package tasks """
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Returns a task """
+    return asyncio.Task(wait_random(max_delay))
