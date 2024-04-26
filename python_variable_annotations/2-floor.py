8ab998aaeecea3c2e2c12f0fb8166d169ba4eb54
yene2024 <yeneishla1524@gmail.com>
2024-04-26 11:06:27 -0400

    Fixing code

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
-#!/usr/bin/ env python3
-"""Takes a float n as argument and returns
-the floor of the float"""
+#!/usr/bin/env python3
+"""Takes a float n as argument and returns the floor of the float"""
 import math
 
 
 def floor(n: float) -> int:
-    """Returns the floor of the input float."""
-    return math.floor(n)
+    """return floor of a float"""
+    result = int(math.floor(n))
+    return result
