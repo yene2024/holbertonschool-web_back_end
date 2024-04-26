62fa052e1fa85c39d8a26184eacedc61daabd9ca
yene2024 <yeneishla1524@gmail.com>
2024-04-26 11:08:02 -0400

    Adding documentation

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!usr/bin/env python3
+"""Function concat takes str1 and str2 and
+returns concatenated string"""
 def concat(str1: str, str2: str) -> str:
     """Concatenates two strings and returns the result."""
     return str1 + str2
