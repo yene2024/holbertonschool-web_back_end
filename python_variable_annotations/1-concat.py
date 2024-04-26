8116690e03f25a328179d586dddfd07ba9af574d
yene2024 <yeneishla1524@gmail.com>
2024-04-26 11:08:42 -0400

    Fixing code

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
-#!usr/bin/env python3
-"""Function concat takes str1 and str2 and
-returns concatenated string"""
+#!/usr/bin/env python3
+"""Function concat takes str1 and str2 and returns concatenated string"""
+
+
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings and returns the result."""
+    """sum str1 and str2"""
     return str1 + str2
