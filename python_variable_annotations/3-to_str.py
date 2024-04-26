272e2c1ec6f32232a25d93fb880049146149773f
yene2024 <yeneishla1524@gmail.com>
2024-04-26 11:25:47 -0400

    Adding documentation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Takes a float n as argument and returns
+the string representation"""
+
+
 def to_str(n: float) -> str:
     """Converts a float to its string representation."""
     return str(n)
