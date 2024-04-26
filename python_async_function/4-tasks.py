0c62a2c96a194864fc0aadaace4ce741503acd2e
yene2024 <yeneishla1524@gmail.com>
2024-04-26 12:00:25 -0400

    Take the code from wait_n and alter it into a new function

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Mutiple coroutines """
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Using as completed """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
