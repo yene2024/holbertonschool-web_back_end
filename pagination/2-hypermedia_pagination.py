17baf09e5a0d3ac55562a4239ae6c167d33551a8
yene2024 <yeneishla1524@gmail.com>
2024-05-03 14:01:17 -0400

    Implementing a get_hyper method that takes the same arguments

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""A get_hyper method that takes the same arguments
+(and defaults) as get_page and returns a dictionary
+containing key-value pairs"""
+import csv
+from typing import List
+import math
+
+
+def index_range(page, page_size):
+    """
+    Calculate start and end index for pagination parameters
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return a dictionary of pagination data"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
