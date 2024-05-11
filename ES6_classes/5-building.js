1ed79e6ae040dadc16e78e7b9196a64ae402b6e6
yene2024 <yeneishla1524@gmail.com>
2024-05-10 21:36:59 -0400

    Implementing a class named Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+class Building {
+    constructor(sqft) {
+      if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+  
+      this._sqft = sqft;
+    }
+  
+    // sqft
+    get sqft() {
+      return this._sqft;
+    }
+  }
+  
+  export default Building;
