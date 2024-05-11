ed7ea14058184e1053a930b24b8174822640bd73
yene2024 <yeneishla1524@gmail.com>
2024-05-10 21:35:46 -0400

    Importing the ClassRoom class from 0-classroom.js.

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import Classroom from './0-classroom';
+
+function initializeRooms() {
+  return [new Classroom(19), new Classroom(20), new Classroom(34)];
+}
+
+export default initializeRooms;
