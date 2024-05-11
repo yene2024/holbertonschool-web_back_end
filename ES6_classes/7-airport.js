fceb5ccb186075a4cfa5e45bd3b610be26886b2d
yene2024 <yeneishla1524@gmail.com>
2024-05-10 21:37:34 -0400

    Implementing a class named Airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this._code;
+    }
+  }
+  
+  export default Airport;
