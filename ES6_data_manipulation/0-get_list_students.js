10ad2a8836a5f85058004a1b448ab6ffa532dd97
yene2024 <yeneishla1524@gmail.com>
2024-05-23 22:32:19 -0400

    Fixing code

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,11 @@
-export default function getListStudents() {
-    const obj = [
+function getListStudents() {
+    const objects = [
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
       { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' }];
-    return obj;
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+  
+    return objects;
   }
+  
+  export default getListStudents;
